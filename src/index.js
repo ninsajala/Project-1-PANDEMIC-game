f1446275bdbc54736ee0788d5d88228ccbdd3867
Nina van den Braak <ninavandenbraak@gmail.com>
2020-10-26 09:59:10 +0100

    added comments for ideas of how code could work

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//PLAYER
+//Make a class for the player
+//Constructor should contain score
+//Constructor should contain name
+//Should move according to left or right arrow pressed
+//Needs a method
+//should not be able to leave the game area or move up
+//should receive damage when touched by corona
+
+//GAME AREA
+//Make a class for the game area
+//Should have a method that draws the board (called when the start-button is clicked)
+//should contain score
+
+//CORONA
+//Make a class for corona
+//Should 'disappear' when clicked/shot
+//Should receive a (random?) speed
+
+//START BUTTON
+//start button event listener
+//should draw the board
+//should set an interval that will create more and more corona
+//should activate the player
+//should draw the score on the board
