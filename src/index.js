589ec673785afa90cfef6fc5a01b3f08450fde2c
Nina van den Braak <ninavandenbraak@gmail.com>
2020-10-26 16:16:28 +0100

    started defining some of the game components

--- src/index.js
+++ src/index.js
@@ -6,22 +6,82 @@
 //something with frames
 //function update game area
 
+
+class Component {
+    constructor(x, y, width, height) {
+        this.width = width
+        this.height = height
+        this.x = x
+        this.y = y
+        this.speedX = 0
+        this.speedY = 0
+    }
+
+    newPosition() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
 //PLAYER
 //Make a class for the player
-//Constructor should contain score, starting on 0
-//Constructor should contain name
-//Constructor should contain immunity level, starting on 3
-//Should move according to left or right arrow pressed (X decreasing/increasing)
-//should not be able to leave the game area or move up/down
-//should receive damage when touched by corona
-//Should increase score when corona is shot
+class Player extends Component {
+    constructor(x, y, width, height, name) {
+        super(x, y, width, height, speedX)
+        //Constructor should contain name
+        this.name = name
+        //Constructor should contain score, starting on 0
+        this.score = 0
+        //Constructor should contain immunity level, starting on 3
+        this.immunity = 3
+    }
+    //Should move according to left or right arrow pressed (X decreasing/increasing)
+    movePlayer(keycode) {
+        //should not be able to leave the game area or move up/down
+        if (this.x <= 0 || this.x >= 500) {
+            return console.log('Cannot move out of canvas')
+        } else {
+            switch (keycode) {
+                case 39:
+                    return this.speedX += 1;
+                    break;
+                case 37:
+                    return this.speedX -= 1;
+            }
+        }
+    }
+    //should receive damage when touched by corona
+    decreaseImmunity() {
+        return this.immunity -= 1
+    }
+    //Should increase score when corona is shot
+    increaseScore(speed) {
+        return this.score += speed
+    }
+}
 
 //SANITIZER
-//Should be activated by SPACE
 //Moves up (Y-decreasing) when activated
 //Starts on current X of the player
 //Should disappear when meets same position as a corona
 //If it doesn't meet the same position as corona it will move out of the game area
+
+class Sanitizer extends Component {
+    constructor(x, y, width, height) {
+        super(x, y, width, height, speedY)
+    }
+}
+
+function newSanitizerSpray() {
+    return sanitizer = new Sanitizer(player.x, 0, 5, 5)
+}
+
+//Should be activated by SPACE
+document.body.onkeyup = function (e) {
+    if (e.keyCode === 32) {
+        newSanitizerSpray()
+    }
+}
 
 //CORONA
 //Make a class for corona
@@ -38,3 +98,5 @@
 //should activate the player
 //should draw the score on the board
 //should draw the immunity on the board
+
+//Check Game over
