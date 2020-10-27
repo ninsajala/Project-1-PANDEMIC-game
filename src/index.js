4e1a3b38e1e3989a930f2624e9b4989130d9f258
Nina van den Braak <ninavandenbraak@gmail.com>
2020-10-27 21:17:05 +0100

    made player move, made sanitizer work, added images, added more sounds, cleaned up comments and unneccesary code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-//GAME AREA
-//Make a class for the game area
 let player
+let name
 let canvas
 let ctx
 const gameBoard = document.getElementById('game-board')
@@ -12,37 +11,48 @@ const myGameArea = {
         this.canvas.height = 500;
         ctx = this.canvas.getContext("2d");
         gameBoard.appendChild(this.canvas)
-        //call updateGameArea() every 20 milliseconds
-        //updateGameScreen()
         this.interval = setInterval(updateGameScreen, 20);
         this.frameNo = 0;
-        //loads the spray sound
         this.spraySound = new Sound('../sound-effects/spray-effect.mp3')
+        this.cleanSound = new Sound(`../sound-effects/clean-effect.mp3`)
+        this.
         updateScoreScreen(player)
         updateImmunityScreen(player)
     },
-    // clear the canvas before starting new draw
+
     clear: function () {
-        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        let img = new Image()
+        img.src = '../images/background-option.jpeg'
+        ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
     },
     stop: function () {
         clearInterval(this.interval);
     }
 }
 
+function updateGameScreen() {
+    myGameArea.clear(); //clears the canvas to print new graphic
+    player.newPosition(); //places player on new coordinates
+    player.update(); //puts canvas of player on screen
+    //drawVirus(); //will create new virus obstacles
+    //checkGameOver(); // immunity=0
+    updateScoreScreen(player);
+    updateImmunityScreen(player);
+    updateSanitizers()
+}
+
 function updateScoreScreen(player) {
-    ctx.font = "18px Creepster";
+    ctx.font = "20px Creepster";
     ctx.fillStyle = "black";
     ctx.fillText(`Score: ${player.score}`, 400, 20);
 }
 
 function updateImmunityScreen(player) {
-    ctx.font = "18px Creepster";
+    ctx.font = "20px Creepster";
     ctx.fillStyle = "black";
     ctx.fillText(`Immunity: ${player.immunity}`, 400, 50);
 }
 
-//Class for all sounds
 function Sound(src) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
@@ -58,37 +68,27 @@ function Sound(src) {
     }
 }
 
-//PLAYER
-//Make a class for the player
 class Player {
-    constructor(name) {
-        this.x = 260
-        this.y = 450
+    constructor() {
+        this.x = 240
+        this.y = 425
         this.width = 50
         this.height = 75
-        //Can move from right to left
         this.speedX = 0
-        //Constructor should contain name
-        this.name = name
-        //Constructor should contain score, starting on 0
         this.score = 0
-        //Constructor should contain immunity level, starting on 3
         this.immunity = 3
-        //this.img = new Image()
-        //this.img.src = `../images/bottle.png`
     }
+
     newPosition() {
         this.x += this.speedX
     }
 
-    //Makes component move
     update() {
-        ctx.fillStyle = 'red'
-        ctx.fillRect(this.x, this.y, this.width, this.height)
-        //ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+        let img = new Image()
+        img.src = `../images/bottle.png`
+        ctx.drawImage(img, this.x, this.y, this.width, this.height)
     }
 
-    //Find positions for crashing logic
     left() {
         return this.x
     }
@@ -102,7 +102,6 @@ class Player {
         return this.y + this.height
     }
 
-    //crashing logic
     crashWith(object) {
         if (
             this.top() === object.bottom() &&
@@ -114,35 +113,41 @@ class Player {
         }
     }
 
-
-    //Should move according to left or right arrow pressed (X decreasing/increasing)
     movePlayer(keyCode) {
-        //should not be able to leave the game area or move up/down
-        if (this.x <= 0 || this.x >= 500) {
-            this.speedX = 0
-        } else {
-            switch (keyCode) {
-                case 39:
-                    return this.speedX += 1;
-                    break;
-                case 37:
-                    return this.speedX -= 1;
-            }
+        switch (keyCode) {
+            case 39:
+                if (this.x <= 470) {
+                    return this.speedX += 4
+                };
+                break;
+            case 37:
+                if (this.x >= 5) {
+                    return this.speedX -= 4;
+                }
         }
     }
-    //should receive damage when touched by corona
+
     decreaseImmunity() {
         return this.immunity -= 1
     }
-    //Should increase score when corona is shot
+
     increaseScore(speed) {
         return this.score += speed
     }
 }
 
-//Event listener that makes player move
-document.addEventListener('keyup', function (e) {
+document.addEventListener('keydown', function (e) {
     player.movePlayer(e.keyCode)
+})
+
+document.addEventListener('keyup', (e) => {
+    switch (e.keyCode) {
+        case 39:
+            return player.speedX = 0
+            break;
+        case 37:
+            return player.speedX = 0
+    }
 })
 
 //SANITIZER
@@ -150,26 +155,21 @@ class Sanitizer {
     constructor(x) {
         this.x = x
         this.y = 500
-        this.width = 20
-        this.height = 20
-        //Moves up (Y-decreasing) when activated
+        this.width = 30
+        this.height = 30
         this.speedY = 5
-        //this.img = new Image()
-        //this.img.src = `../images/sanitizer-spray.png`
     }
 
     newPosition() {
         this.y -= this.speedY
     }
 
-    //Makes component move
     update() {
-        ctx.fillStyle = 'green'
-        ctx.fillRect(this.x, this.y, this.width, this.height)
-        //ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+        let img = new Image()
+        img.src = `../images/sanitizercloud.png`
+        ctx.drawImage(img, this.x, this.y, this.width, this.height)
     }
 
-    //Find positions for crashing logic
     left() {
         return this.x
     }
@@ -183,7 +183,6 @@ class Sanitizer {
         return this.y + this.height
     }
 
-    //crashing logic
     crashWith(object) {
         if (
             this.top() === object.bottom() &&
@@ -195,26 +194,22 @@ class Sanitizer {
         }
     }
 }
-//array that will keep track of all created sanitizers
+
 const allSanitizers = []
 
-//function that will create a new sanitizer component
 function newSanitizerSpray() {
     myGameArea.spraySound.play()
-    //setTimeout(myGameArea.spraySound.stop(), 1000)
     let sanitizer = new Sanitizer(player.x)
     allSanitizers.push(sanitizer)
 }
 
-//Sanitizer should be activated by SPACE
 document.addEventListener('keydown', function (e) {
+    e.preventDefault()
     if (e.keyCode === 32) {
         newSanitizerSpray()
     }
 })
 
-//Should disappear when meets same position as a corona + increase score of player
-//IN PROGRESS
 const sanitized = allSanitizers.some(function (object) {
     return sanitizer.crashWith(object)
 })
@@ -222,6 +217,7 @@ const sanitized = allSanitizers.some(function (object) {
 function checkIfSanitized() {
     for (let i = 0; i < coronas.length; i++) {
         if (sanitized(coronas[i])) {
+            myGameArea.cleanSound.play()
             player.increaseScore()
             allCoronas[i].speedY = 0
             allCoronas[i].width = 0
@@ -236,18 +232,6 @@ function updateSanitizers() {
         allSanitizers[i].update()
     }
 }
-
-function updateGameScreen() {
-    myGameArea.clear(); //clears the canvas to print new graphic
-    player.newPosition(); //places player on new coordinates
-    player.update(); //puts canvas of player on screen
-    //drawVirus(); //will create new virus obstacles
-    //checkGameOver(); // immunity=0
-    updateScoreScreen(player);
-    updateImmunityScreen(player);
-    updateSanitizers()
-}
-
 
 // class Corona {
 //     constructor() {
@@ -293,27 +277,16 @@ function updateGameScreen() {
 //     // }
 // }
 
-
-//let name
-
-// function askPlayerName() {
-//     return name = alert('Hi Corona warrior! What is your name?')
-// }
 //START BUTTON
 const startButton = document.getElementById('start-button')
-//start button event listener
+
 startButton.addEventListener('click', function () {
     startButton.innerHTML = 'Restart'
-    //should draw the board
-
-    //should activate the player
-    //askPlayerName()
-    player = new Player('Piet')
+    player = new Player()
     //setupVirus();
     myGameArea.start()
 })
 
 //To do:
 //Write game over function that checks if immunity is 0 and stops interval if true
-//Update sanitizer function --> for loop
 //Player crash with corona function --> call decrease immunity
